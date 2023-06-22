d9a915aeaacebeceb56da16102ed5cbec53b3771
Katerina <katena.moskvina@bk.ru>
2023-06-22 07:16:16 +0600

    кнопки

--- main.py
+++ main.py
@@ -10,20 +10,33 @@ S_16BIT = 2 ** 16
 OCT_NUMBER = 3
 OCTAVES = ["contr", "greate", "small", "first", "second", "third", "fourth"]
 
+NOTES = ["C1", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Hb", "H", "C2"]
+
 GENERATION_TYPE = "sinus"
 GENERATION_TYPES = ["sinus", "saw", "guitar"]
 EFFECTS = {"distortion": 1}
 
-
+def change_gen():
+    pass
+def set_dist():
+    pass
+def play_note(note):
+    print(note)
 
 dpg.create_context()
 
 with dpg.window(label="FL", tag="Primary Window"):
-    dpg.add_text(f"{OCTAVES[OCT_NUMBER]}")
-    dpg.add_button(label=f"{GENERATION_TYPES[GENERATION_TYPE]}", callback=)
+    oct_label = dpg.add_text(f"{OCTAVES[OCT_NUMBER]}")
+    change_gen_btn = dpg.add_button(label=f"{GENERATION_TYPE}", callback=change_gen)
+
     dpg.add_text("Distortion")
-    dpg.add_button(label="Set", callback=)
-    dpg.add_slider_float(label="", default_value=0.1, min_value=0.1, max_value=1)
+    dpg.add_button(label="Set", callback=set_dist)
+    dist_slider = dpg.add_slider_float(label="", default_value=0.1, min_value=0.1, max_value=1)
+
+    buttons = []
+    for i in NOTES:
+        print(i)
+        buttons.append(dpg.add_button(label=i, callback=play_note))
 
 dpg.create_viewport(title='Custom Title', width=600, height=300)
 dpg.setup_dearpygui()
